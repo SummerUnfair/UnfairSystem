23ca4aa02344e230affeaa26d70447442e2d26e4
SummerUnfair <928971634@qq.com>
2020-05-29 21:42:29 +0800

    解决.gitignore未生效问题

---
+++ src/main/java/com/unfair/UnfairSystemApplication.java
@@ -0,0 +1,13 @@
+package com.unfair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnfairSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnfairSystemApplication.class, args);
+    }
+
+}
